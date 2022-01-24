734a862679540064e56add98e87db81f16de5fa0
Reinhard Kiesswetter <reinhard@scerus.com>
2022-01-24 16:39:17 +0100

    update to lates ts version

--- tailscale-install-up.ps1
+++ tailscale-install-up.ps1
@@ -5,7 +5,7 @@ param (
     $authkey
 )
 mkdir C:\temp -Force
-wget https://pkgs.tailscale.com/stable/tailscale-ipn-setup-1.18.0.exe  -OutFile C:\temp\tailscale-ipn-setup-1.18.0.exe
+wget https://pkgs.tailscale.com/stable/tailscale-ipn-setup-1.20.2.exe  -OutFile C:\temp\tailscale-ipn-setup-1.20.2.exe
 Start-Process -FilePath C:\temp\tailscale-ipn-setup-1.18.0.exe -Wait -NoNewWindow -ArgumentList ' /S'
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 tailscale up --authkey=$authkey --unattended --accept-routes
